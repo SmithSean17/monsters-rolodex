9ef8727d95f4ded6fab1cefd13b59daa2e9ba6f3
Sean <Smith.seanm@yahoo.com>
2020-05-17 12:51:44 -0700

    Updates

---
+++ precache-manifest.3a01f8fb1ac299d51e68749c5ef9a1c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "583319061ff2395caec6ebdcf8785b71",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3182277597edf4378f00",
+    "url": "/monsters-rolodex/static/css/main.4a06214b.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3182277597edf4378f00",
+    "url": "/monsters-rolodex/static/js/main.803e1be3.chunk.js"
+  },
+  {
+    "revision": "e3f9453037e5f28f8167",
+    "url": "/monsters-rolodex/static/js/runtime-main.7c0f1f0f.js"
+  }
+]);
